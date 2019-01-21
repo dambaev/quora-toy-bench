8c7cff39495dbda23665f6798c5add2464dcc87c
Dambaev Alexander <ice.redmine@gmail.com>
2019-01-22 01:38:25 +0800

    reorganize benchmarks

--- haskell/bench/first.hs
+++ haskell/bench/first.hs
@@ -12,10 +12,14 @@ main = do
   putStrLn $ "threads avaialble: " ++ show threads
   defaultMain
     [ bench ( "idiomatic fork " ++ show threads) $ nfIO $ Lib.calcFork threads
+    , bench ( "ffi fork " ++ show threads ) $ nfIO $ Lib1.calcFork threads
+
     , bench ( "idiomatic async " ++ show threads) $ nfIO $ Lib.calcAsync threads
+    , bench ( "ffi async " ++ show threads ) $ nfIO $ Lib1.calcAsync threads
+
     , bench ( "idiomatic parallel " ++ show threads) $ nf Lib.calcParallel threads
+    , bench ("ffi parallel " ++ show threads) $ nf Lib1.calcParallel threads
+
     , bench "idiomatic single" $ nf (Lib.calcPart Lib.n) 0
     , bench "ffi single" $ nfIO Lib1.calcSingleFFI
-    , bench ("ffi parallel " ++ show threads) $ nf Lib1.calcParallel threads
-    , bench ( "ffi async " ++ show threads ) $ nfIO $ Lib1.calcAsync threads
     ]
